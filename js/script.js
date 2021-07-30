46b7863cd839bf2f1ed57147f926183747a129df
maldrek3103 <cpisanu96@gmail.com>
2021-07-30 16:58:51 +0200

    added little bit of HTML and CSS

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var root = new Vue({
+    el: '#root',
+    data: {
+        todos: [
+            { text: 'Comprare il pane' },
+            { text: 'Dare da mangiare al pesce rosso' },
+            { text: 'Fare un buca in giardino' },
+            { text: 'Tirare sassi in mare' },
+        ]
+    }
+})
