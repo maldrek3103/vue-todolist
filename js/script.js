0124290ab9a9b94c854def3c302d3f371432e917
maldrek3103 <cpisanu96@gmail.com>
2021-08-30 16:10:54 +0200

    Finished todo app

--- js/script.js
+++ js/script.js
@@ -1,16 +1,58 @@
-var root = new Vue({
+const root = new Vue({
     el: '#root',
     data: {
+        newTodo: '',
+        searchTerm: '',
         todos: [
-            { text: 'Comprare il pane' },
-            { text: 'Dare da mangiare al pesce rosso' },
-            { text: 'Fare un buca in giardino' },
-            { text: 'Tirare sassi in mare' },
-        ]
+            {
+                text: 'Comprare il pane',
+                done: false,
+            },
+            {
+                text: 'Dare da mangiare al pesce rosso',
+                done: false,
+            },
+            {
+                text: 'Fare un buca in giardino',
+                done: false,
+            },
+            {
+                text: 'Tirare sassi in mare',
+                done: false,
+            },
+        ],
     },
     methods: {
-        deleteTask(index) {
+        deleteTodo(index) {
+            this.todos.splice(index, 1);
+        },
+        addTodo() {
+            if (this.newTodo.trim()) {
+                this.todos.push({ text: this.newTodo, done: false });
+            }
+            this.newTodo = '';
 
-        }
-    }
-})
+        },
+        showItem(text) {
+            if (!this.searchTerm || this.searchTerm.trim() === "") {
+                return true;
+            };
+            const filter = this.searchTerm.trim().toLowerCase();
+            text = text.toLowerCase();
+            return text.includes(filter);
+        },
+        toggleDone(index) {
+            const updatedTodo = this.todos.map((todo, todoIndex) => {
+                if (todoIndex === index) {
+                    todo.done = !todo.done;
+                }
+                return todo;
+            });
+            this.todos = updatedTodo;
+
+        },
+        isDone(index) {
+            return this.todos[index].done;
+        },
+    },
+});
